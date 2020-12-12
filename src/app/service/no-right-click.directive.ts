a503b1f6cb78ce24ce2246149790de2bbce5a606
Shasanka Isuru <shasanka.isuru@dialog.lk>
2020-12-12 11:13:18 +0530

    fixes

--- src/app/service/no-right-click.directive.ts
+++ src/app/service/no-right-click.directive.ts
@@ -11,9 +11,12 @@ export class NoRightClickDirective {
     }
 
     @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+      console.log(event)
         if (event.code === 'F12') {
           event.preventDefault();
-        }
+        } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+          event.preventDefault();
+      }
     }
 
   constructor() { }
