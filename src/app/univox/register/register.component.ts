be40c779d85eaefd79abb1d9fd7ebe5570b3be1c
Shasanka Isuru <shasanka.isuru@dialog.lk>
2020-12-15 21:42:48 +0530

    update

--- src/app/univox/register/register.component.ts
+++ src/app/univox/register/register.component.ts
@@ -460,7 +460,7 @@ public alattemt = [
       exam_year: this.registrationForm.value.year.name,
       z_score: this.registrationForm.value.zscore,
       attempt: this.registrationForm.value.attempt.name,
-      stream: this.registrationForm.value.stream,
+      stream: this.registrationForm.value.stream.name,
       sub_1: this.registrationForm.value.subject1.name,
       sub_2: this.registrationForm.value.subject2.name,
       sub_3: this.registrationForm.value.subject3.name,
